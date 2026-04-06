c8e2318515bf416eeba49bf37bd13b9b06de46ee
Aswini97 <aswinipanigrahi6@gmail.com>
2026-04-07 00:20:14 +0530

    Updated Services

--- ktinoskare/settings.py
+++ ktinoskare/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'corsheaders'
+    'corsheaders',
     'rest_framework',
     'drf_spectacular',
     "channels",
